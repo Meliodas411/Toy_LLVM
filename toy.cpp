39b17978b0369fbfaed173b437adcddc5cff2f95
hkzwhn1314 <Meliodas.zhaocen@gmail.com>
2023-04-25 10:22:10 -0400

    new toy use llvm to desine new language

---
+++ toy.cpp
@@ -0,0 +1,71 @@
+#include <string>
+// no use namespace
+
+enum Token_Type {
+  EOF_TOKEN = 0,    // EOF
+  NUMERIC_TOKEN,    // NUM
+  IDENTIFIER_TOKEN, // id
+  PARAN_TOKEN,      // {}
+  DEF_TOKEN         // def func
+};
+
+// ===========define
+static int Numeric_Val;
+static std::string Identifier_string;
+static FILE *file;
+// ===========end of define
+
+static int get_token() {
+  static int LastChar = ' ';
+  while (isspace(LastChar))
+    LastChar = fgetc(file);
+  if (isalpha(LastChar)) {
+    Identifier_string = LastChar;
+    while (isalnum((LastChar = fgetc(file))))
+      Identifier_string += LastChar;
+    if (Identifier_string == "def")
+      return DEF_TOKEN;
+    return IDENTIFIER_TOKEN;
+  }
+  if (isdigit(LastChar)) {
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = fgetc(file);
+    } while (isdigit(LastChar));
+    Numeric_Val = strtod(NumStr.c_str(), 0);
+    return NUMERIC_TOKEN;
+  }
+  if (LastChar == '#') {
+    do
+      LastChar = fgetc(file);
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+    if (LastChar != EOF)
+      return get_token();
+  }
+  if (LastChar == EOF)
+    return EOF_TOKEN;
+  int ThisChar = LastChar;
+  LastChar = fgetc(file);
+  return ThisChar;
+}
+
+// ===========AST tree
+
+// base ast
+class BaseAST {
+public:
+  virtual ~BaseAST();
+};
+
+// 变量表达式的 AST 类定义
+class VariableAST : public BaseAST {};
+// 数值表达式的 AST 类定义
+class NumericAST : public BaseAST {};
+// 二元运算组成的表达式
+class BinaryAST : public BaseAST {};
+// 用于函数声明的 AST 类定义
+class FunctionDeclAST {};
+
+class FunctionDefnAST {};
+class FunctionCallAST : public BaseAST {};
